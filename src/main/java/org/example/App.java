17c2a97004bf56e2c9d2e646b3996012ac0051e0
carloronconi <carlo.ronconi@mail.polimi.it>
2023-10-02 11:20:12 +0200

    Add http client example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,46 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "What's your name?" );
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println("Hello, " + input + "!");
+
+        HttpRequest requestGet = null;
+        try {
+            requestGet = HttpRequest.newBuilder()
+                    .uri(new URI("https://postman-echo.com/get"))
+                    .header("key1", "value1")
+                    .header("key2", "value2")
+                    .GET()
+                    .build();
+
+            HttpRequest requestPost = HttpRequest.newBuilder()
+                    .uri(new URI("https://postman-echo.com/post"))
+                    .headers("Content-Type", "text/plain;charset=UTF-8")
+                    .POST(HttpRequest.BodyPublishers.ofString("Sample request body"))
+                    .build();
+
+            HttpClient client = HttpClient.newBuilder().build();
+
+            HttpResponse<String> response = client.send(requestGet, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.body());
+
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
