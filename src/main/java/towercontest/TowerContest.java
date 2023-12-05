92ea2741c7fb27e758bac8cab2da086a32d1145b
carloronconi <carlo.ronconi@mail.polimi.it>
2023-12-05 09:16:28 +0100

    Complete Tower Contest request description

--- src/main/java/towercontest/TowerContest.java
+++ src/main/java/towercontest/TowerContest.java
@@ -7,8 +7,8 @@ import java.util.List;
  * You have built some matchstick towers. A jury will judge the towers, and you know their grade is highly correlated
  * with the height of the towers. Towers have unique names. Towers enter and exit the judging arena at some schedules
  * (always assumed correct). You want to estimate your chances of victory by returning, among the tallest towers currently in
- * the arena, only those that you own. You are given the below signatures only and have to complete the class. The main
- * method is provided.
+ * the arena, only those that you own. You are given the classes TowerDescription, TowerSchedule and the below signatures
+ * and have to complete the class TowerContest. The main method of TowerContest is provided.
  */
 public class TowerContest {
     public TowerContest(List<TowerDescription> yourTowers, List<TowerSchedule> yourSchedules) {
