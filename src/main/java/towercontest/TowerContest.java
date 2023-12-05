3596505bd0d6251a38b34089f33690bedfe11402
carloronconi <carlo.ronconi@mail.polimi.it>
2023-12-05 11:06:52 +0100

    Improve problem statement

--- src/main/java/towercontest/TowerContest.java
+++ src/main/java/towercontest/TowerContest.java
@@ -7,9 +7,10 @@ import java.util.stream.Collectors;
  * You have built some matchstick towers. A jury will judge the towers, and you know their grade is highly correlated
  * with the height of the towers. Towers have unique names. Towers enter and exit the judging arena at some schedules
  * (always assumed correct). You want to estimate your chances of victory by returning, among the tallest towers currently in
- * the arena, only those that you own. You are given the classes TowerDescription, TowerSchedule and the below signatures
- * and have to complete the class TowerContest. The tests are provided (and are very few so try to cover corner cases even
- * if not covered by tests)
+ * the arena, only those that you own. You are given: the classes TowerDescription, TowerSchedule and the signatures of the
+ * constructor and the methods towerEntered, towerExited, tallestCurrentTowersOwned of class TowerContest. You should
+ * complete the class TowerContest. The tests are provided (and are very few so try to cover corner cases even
+ * if not covered by tests).
  */
 public class TowerContest {
     private final List<TowerInfo> myTowers;
